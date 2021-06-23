aeebfbd26bb5e22732ff9764275acbe7e98a9a24
r-svp26 <riteshk6341@gmail.com>
2021-06-24 00:45:19 +0530

    Added: To store values in Dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -4,4 +4,17 @@ read -p "Enter the first number:" a
 read -p "Enter the second number:" b
 read -p "Enter the third number:" c
 
-echo $((a%b+c))
+result1=$(($a+$b*$c))
+result2=$(($a*$b+$c))
+result3=$(($c+$a/$b))
+result4=$(($a%$b+$c))
+
+
+declare -A result
+
+result[m]=$result1
+result[n]=$result2
+result[o]=$result3
+result[p]=$result4
+
+echo ${result[@]}
